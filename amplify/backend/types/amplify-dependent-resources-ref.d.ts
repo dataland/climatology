5dd1efc6eec9b1c1198076590c5f504e6818a153
EC2 Default User <ec2-user@ip-172-31-26-128.eu-central-1.compute.internal>
2024-02-03 10:48:26 +0000

    added graph

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "climatology": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "climatology644807c8": {
       "AppClientID": "string",
